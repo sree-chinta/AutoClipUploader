a230105f65b0307f45ec178e8ba068cd2f49b6e1
Samik Pradhan <samikpradhan@gmail.com>
2024-09-27 15:18:51 -0700

    gets files based on the folder

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
+import googleapiclient.discovery
+from googleapiclient.http import MediaFileUpload
 from tkinter import Tk
 from tkinter.filedialog import askdirectory
 
@@ -32,8 +34,15 @@ def main():
     path = askdirectory(title="Select Folder")
     print(path)
 
-    vid_metadata = getUserInput()
-    print(vid_metadata)
+    files = os.listdir(path)
+    for file in files:
+        if os.path.isdir(os.path.join(path, file)):
+            files.remove(file)
+    print(files)
+    print()
+
+    # vid_metadata = getUserInput()
+    # print(vid_metadata)
 
 
 if __name__ == "__main__":
