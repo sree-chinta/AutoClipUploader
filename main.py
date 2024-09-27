b5302afd24670c5435ba6ab85b96086ee27d226a
Samik Pradhan <samikpradhan@gmail.com>
2024-09-27 14:53:12 -0700

    added user input code from test.py into main.py as a function that returns video metadata

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
+import os
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
 from tkinter import Tk
 from tkinter.filedialog import askdirectory
+
+
+def getUserInput():
+    # Prepare the video metadata
+    title = input("Enter Video Title: ")
+    description = input("Enter the description of the video: ")
+    tags = list(input("Enter the tags: ").split())
+    categoryId = input("Enter the category: ")
+    privacy = input("Enter the video's privacy (public, private, unlisted): ")
+    video_metadata = {
+        "snippet": {
+            "title": title,
+            "description": description,
+            "tags": tags,
+            "categoryId": categoryId,  # "22" is the category for "People & Blogs"
+        },
+        "status": {
+            "privacyStatus": privacy,  # Options: "public", "private", "unlisted"
+        },
+    }
+
+    return video_metadata
 
 
 def main():
     path = askdirectory(title="Select Folder")
     print(path)
+
+    vid_metadata = getUserInput()
+    print(vid_metadata)
 
 
 if __name__ == "__main__":
